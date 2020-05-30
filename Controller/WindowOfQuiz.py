0dc9d3281e45828552ef0ce600fb1a4e282df1ac
AdrianDevid <adriandevid36@gmail.com>
2020-05-30 18:03:31 -0300

    First Commit

--- Controller/WindowOfQuiz.py
+++ Controller/WindowOfQuiz.py
@@ -1,4 +1,20 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 class WindowOfQuiz(Screen):
-    pass
+    ListOfVerification = []
+    ListQuest = []
+    def RespostQuizInBD(self,valueAnswer):
+        if valueAnswer == 'Sim':
+            WindowOfQuiz.ListOfVerification.append(1)
+
+            for i in self.ids:
+                cont = 0
+                l = list(i)
+                if cont < 1:
+                    WindowOfQuiz.ListQuest.append(int(l[9]))
+                if WindowOfQuiz.ListQuest[cont] == int(l[9]) or cont < 1:
+                    print("encontrado!!!")
+                    break
+                else:
+                    print("n ppode entrar")
+                cont += 1
--- Controller/config.py
+++
@@ -1,2 +0,0 @@
-EMAIL_ADDRESS = "eth0.ifconfig@gmail.com"
-PASSWORD= "x3Ur7hrO%2BVr3RH"
