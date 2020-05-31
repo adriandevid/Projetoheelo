15f1103e6c26b6414ec63bbc2aaf2853090e1bf4
AdrianDevid <adriandevid36@gmail.com>
2020-05-31 15:10:05 -0300

    First Commit

--- Controller/WindowOfQuiz.py
+++ Controller/WindowOfQuiz.py
@@ -1,20 +1,39 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
+from Screens.EfectsKv import Efectskv
+from kivy.uix.label import Label
 
 class WindowOfQuiz(Screen):
-    ListOfVerification = []
-    ListQuest = []
-    def RespostQuizInBD(self,valueAnswer):
+    ListOfResp = [0,0,0,0,0,0,0,0,0]
+    ListOfVerification = [0,0,0,0,0,0,0,0,0]
+    contador = 0
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def RespostQuizInBD(self, MDId, valueAnswer):
+        NumberPosition = int(MDId[8])
         if valueAnswer == 'Sim':
-            WindowOfQuiz.ListOfVerification.append(1)
-
-            for i in self.ids:
-                cont = 0
-                l = list(i)
-                if cont < 1:
-                    WindowOfQuiz.ListQuest.append(int(l[9]))
-                if WindowOfQuiz.ListQuest[cont] == int(l[9]) or cont < 1:
-                    print("encontrado!!!")
-                    break
+            if self.ListOfResp.count(0) != 0:
+                self.contador += 1
+                if self.ListOfVerification[NumberPosition] != 1: 
+                    self.ListOfVerification[NumberPosition] = 1
+                    self.ListOfResp[NumberPosition] = 1
+                    Efectskv.EfectDialog()
                 else:
-                    print("n ppode entrar")
-                cont += 1
+                    print("ErrorException: Essa pergunta ja foi respondida!!")
+            else:
+                if (self.ListOfResp[NumberPosition] != 2) and (self.ListOfResp[NumberPosition] == 0):
+                    self.ListOfResp[NumberPosition] = 1
+                Efectskv.EfectDialogResultQuiz()
+        elif valueAnswer == 'Não':
+            if self.ListOfResp.count(0) != 0:
+                self.contador += 1
+                if self.ListOfVerification[NumberPosition] != 1: 
+                    self.ListOfVerification[NumberPosition] = 1
+                    self.ListOfResp[NumberPosition] = 2
+                    Efectskv.EfectDialog()
+                else:
+                    print("ErrorException: Essa pergunta ja foi respondida!!")
+            else:
+                if (self.ListOfResp[NumberPosition] != 1) and (self.ListOfResp[NumberPosition] == 0):
+                    self.ListOfResp[NumberPosition] = 2
+                Efectskv.EfectDialogResultQuiz()
+        print(self.ListOfResp)
