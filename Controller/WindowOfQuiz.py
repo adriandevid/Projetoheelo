f374acb953d4b615875922604a484afaf0191ca4
AdrianDevid <adriandevid36@gmail.com>
2020-06-01 16:55:59 -0300

    First Commit

--- Controller/WindowOfQuiz.py
+++ Controller/WindowOfQuiz.py
@@ -23,4 +23,4 @@ class WindowOfQuiz(Screen):
                 self.ListOfResp[NumberPosition] = 2
         print(self.ListOfResp)
     def SubmitRespostInDb(self):
-        DbFactory.DataBase('SQLite').CreateConnector('ola.db').Connect()
+        print(DbFactory.DataBase('SQLite').CreateConnector('ola.db').Connect())
