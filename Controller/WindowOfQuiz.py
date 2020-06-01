1af3570586f44eb3d526079a399511ec01b1716b
AdrianDevid <adriandevid36@gmail.com>
2020-06-01 16:17:59 -0300

    First Commit

--- Controller/WindowOfQuiz.py
+++ Controller/WindowOfQuiz.py
@@ -1,39 +1,26 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
 from Screens.EfectsKv import Efectskv
-from kivy.uix.label import Label
+from DAO.DbFactory import DbFactory
+from kivymd.toast import toast
+
 
 class WindowOfQuiz(Screen):
     ListOfResp = [0,0,0,0,0,0,0,0,0]
-    ListOfVerification = [0,0,0,0,0,0,0,0,0]
     contador = 0
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    def RespostQuizInBD(self, MDId, valueAnswer):
+    def RespostQuiz(self, MDId, valueAnswer):
         NumberPosition = int(MDId[8])
         if valueAnswer == 'Sim':
-            if self.ListOfResp.count(0) != 0:
-                self.contador += 1
-                if self.ListOfVerification[NumberPosition] != 1: 
-                    self.ListOfVerification[NumberPosition] = 1
-                    self.ListOfResp[NumberPosition] = 1
-                    Efectskv.EfectDialog()
-                else:
-                    print("ErrorException: Essa pergunta ja foi respondida!!")
-            else:
-                if (self.ListOfResp[NumberPosition] != 2) and (self.ListOfResp[NumberPosition] == 0):
-                    self.ListOfResp[NumberPosition] = 1
-                Efectskv.EfectDialogResultQuiz()
+            self.contador += 1
+            self.ListOfResp[NumberPosition] = 1
+            toast("A resposta foi computada!!")
+            if (self.ListOfResp[NumberPosition] != 2) and (self.ListOfResp[NumberPosition] == 0):
+                self.ListOfResp[NumberPosition] = 1
         elif valueAnswer == 'Não':
-            if self.ListOfResp.count(0) != 0:
-                self.contador += 1
-                if self.ListOfVerification[NumberPosition] != 1: 
-                    self.ListOfVerification[NumberPosition] = 1
-                    self.ListOfResp[NumberPosition] = 2
-                    Efectskv.EfectDialog()
-                else:
-                    print("ErrorException: Essa pergunta ja foi respondida!!")
-            else:
-                if (self.ListOfResp[NumberPosition] != 1) and (self.ListOfResp[NumberPosition] == 0):
-                    self.ListOfResp[NumberPosition] = 2
-                Efectskv.EfectDialogResultQuiz()
+            self.contador += 1
+            self.ListOfResp[NumberPosition] = 2
+            toast("A resposta foi computada!!")
+            if (self.ListOfResp[NumberPosition] != 1) and (self.ListOfResp[NumberPosition] == 0):
+                self.ListOfResp[NumberPosition] = 2
         print(self.ListOfResp)
+    def SubmitRespostInDb(self):
+        DbFactory.DataBase('SQLite').Createconnector('ola.db').Connect()
